903e45edb1c2d2a480175270528c2894f74c634d
Patrick Browne <pt.browne@gmail.com>
2018-09-20 17:08:32 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Button } from 'cozy-ui/react'
+
+const node = document.querySelector('#app')
+render(<div>Heello world !</div>, node)
